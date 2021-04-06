1de118db7ef0261bfb653d70e9952e6d2e2f4d1b
ManjaBadel <m.badel@gmx.de>
2021-04-06 20:01:46 +0200

    Created basic ask and question function and vieq

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def home
+  end
+
+  def answer
+    @question = params[:question]
+    if params[:question].end_with?("?")
+      @answer = "Silly question, get dressed and go to work!"
+    elsif params[:question] == "I am going to work"
+      @answer = "Great!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
